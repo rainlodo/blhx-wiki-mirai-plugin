f52ac0a4612c8507b3d1b2b5340222d1f3ae2aae
iris <2270816244@qq.com>
2022-05-17 11:00:56 +0800

    fix : 特殊配装中的红叉叉图标显示bug

--- src/main/kotlin/paint/component/ShipEquipComponent.kt
+++ src/main/kotlin/paint/component/ShipEquipComponent.kt
@@ -3,6 +3,7 @@ package org.iris.wiki.paint.component
 import org.iris.wiki.Checker.draw
 import org.iris.wiki.data.ShipEquipData
 import org.iris.wiki.utils.ImageUtil
+import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.image.BufferedImage
 
@@ -29,6 +30,7 @@ class ShipEquipComponent(
                                     20F, Color.BLACK).init()
         g2.drawImage(title.draw(), 10, 10, null)
 
+        // 基础配装
         data.equipLists.forEach { list ->
 
             if (list.isEmpty()) return@forEach
@@ -57,6 +59,7 @@ class ShipEquipComponent(
             boxX += margin + labelWidth
         }
 
+        // 特殊配装
         val specialEquipLength = 100
         val equipIconLength = width - 300
         data.specialEquipList.forEach {
@@ -70,17 +73,28 @@ class ShipEquipComponent(
             g2.drawImage(tagTextComponent.draw(), margin, boxY + 10, null)
             g2.drawImage(TextComponent("推荐人：${it.recommender}", 20F, Color.BLACK).init().draw(), equipIconLength + 10, boxY + 10, null)
 
-            var equipIconHeight = 40
+            var lastIconStart = boxX // 记录上一个icon的起始位置，用于处理红叉叉图标，详见埃吉尔的wiki特殊配装页面
             it.list.forEach { icon ->
-                val iconTextComponent = TextComponent(icon.name, 20F, Color.BLACK).init()
-                if (boxX + 46 + iconTextComponent.getComponentWidth() > equipIconLength) {
-                    boxX = specialEquipLength
-                    boxY += 42
+                if (icon.name != "红叉叉图标") {
+                    // 普通的特殊配装
+                    val iconTextComponent = TextComponent(icon.name, 20F, Color.BLACK).init()
+                    if (boxX + 46 + iconTextComponent.getComponentWidth() > equipIconLength) {
+                        boxX = specialEquipLength
+                        boxY += 42
+                    }
+                    lastIconStart = boxX
+                    g2.drawImage(ImageUtil.getImage(icon.url), boxX, boxY, 40, 40, null)
+                    boxX += 43
+                    g2.drawImage(iconTextComponent.draw(), boxX, boxY + (40 - iconTextComponent.getComponentHeight()) / 2, null)
+                    boxX += iconTextComponent.getComponentWidth() + 3
                 }
-                g2.drawImage(ImageUtil.getImage(icon.url), boxX, boxY, 40, 40, null)
-                boxX += 43
-                g2.drawImage(iconTextComponent.draw(), boxX, boxY + (40 - iconTextComponent.getComponentHeight()) / 2, null)
-                boxX += iconTextComponent.getComponentWidth() + 3
+                else {
+                    // 红叉叉图标(被划掉的特殊配装)
+                    g2.drawImage(ImageUtil.getImage(icon.url), lastIconStart, boxY, 40, 40, null)
+                    g2.color = Color.RED
+                    g2.stroke = BasicStroke(4f)
+                    g2.drawLine(lastIconStart+43, boxY+18, boxX-3, boxY+18)
+                }
             }
             boxY += 42
             g2.drawImage(TextComponent("推荐理由", 20F, Color.BLACK).init().draw(), margin, boxY + 10, null)
